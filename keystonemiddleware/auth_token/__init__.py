f4bf85648b335fa83bf8f43f21944f30e98eb68a
Rudolf Vriend <rudolf.vriend@sap.com>
2019-01-09 16:05:02 +0100

    added interface override support for keystoneauth's client

--- keystonemiddleware/auth_token/__init__.py
+++ keystonemiddleware/auth_token/__init__.py
@@ -945,7 +945,8 @@ class AuthProtocol(BaseAuthProtocol):
             self.log,
             adap,
             include_service_catalog=self._include_service_catalog,
-            requested_auth_version=auth_version)
+            requested_auth_version=auth_version,
+            interface=self._conf.get('auth_interface'))
 
     def _create_oslo_cache(self):
         # having this as a function makes test mocking easier
