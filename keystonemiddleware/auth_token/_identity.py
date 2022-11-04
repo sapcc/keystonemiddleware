c4641339a601741f78b284355979f3548337d75b
Maurice Escher <maurice.escher@sap.com>
2022-11-04 16:48:10 +0100

    reduce log level of 'Bad response code while validating token'
    It raises a ServiceError anyhow.
    logging with error is too high and produces duplicated issues (e.g. in sentry)

--- keystonemiddleware/auth_token/_identity.py
+++ keystonemiddleware/auth_token/_identity.py
@@ -172,7 +172,7 @@ class IdentityServer(object):
                     'fetch token data')
             raise ksm_exceptions.ServiceError(msg)
         except ksa_exceptions.HttpError as e:
-            self._LOG.error(
+            self._LOG.info(
                 'Bad response code while validating token: %s %s',
                 e.http_status, e.message)
             if hasattr(e.response, 'text'):
